2b42a115b459a79dcc4bf55c8d9fc9cb3183de7d
vamsiGangula <149783303+vamsiGangula@users.noreply.github.com>
2025-08-07 12:55:51 +0000

    new

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
 // Middleware setup
 app.use(cors());
 app.use(bodyParser.json());
